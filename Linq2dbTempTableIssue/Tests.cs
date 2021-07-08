305b2ddbf979a4f275439bb8db1aebd1640a1dd2
Nathan Whittle <nathan.whittle@colheli.com>
2021-07-08 13:02:46 -0700

    Add new test

--- Linq2dbTempTableIssue/Tests.cs
+++ Linq2dbTempTableIssue/Tests.cs
@@ -37,7 +37,7 @@ namespace Linq2dbTempTableIssue
             var transaction = connection.BeginTransaction();
 
             var tempTable = connection.CreateTempTable(new List<Person> {personCopy},
-                new BulkCopyOptions {KeepIdentity = true}, "PersonUpdate");
+                new BulkCopyOptions {KeepIdentity = true}, "PersonUpdate", tableOptions: TableOptions.IsTemporary);
 
             transaction.Commit();
 
@@ -73,12 +73,55 @@ namespace Linq2dbTempTableIssue
             var transaction = connection.BeginTransaction();
 
             var tempTable = connection.CreateTempTable(new List<Person> {personCopy},
+                new BulkCopyOptions {KeepIdentity = true}, "PersonUpdate", tableOptions: TableOptions.IsTemporary);
+
+            transaction.Commit();
+
+            var firstPerson = tempTable.First();
+
+            firstPerson.Name.Should().Be(personCopy.Name);
+            firstPerson.Version.Should().BeEquivalentTo(personCopy.Version, options => options.WithStrictOrdering());
+        }
+
+        [Fact]
+        public void
+            BulkCopy_Inserts_Correctly_When_Id_Exists_In_Permanent_Table_With_Same_Model_Type_With_Extra_Entity()
+        {
+            var factory = new EFCoreSqliteInMemoryDbFactory();
+            var context = factory.CreateDbContext<MainContext>();
+
+            var person = new Person
+            {
+                Name = "John Doe"
+            };
+
+            var person2 = new Person
+            {
+                Name = "Jean Doe"
+            };
+
+            context.AddRange(person, person2);
+            context.SaveChanges();
+
+            var personCopy = new Person
+            {
+                Id = person.Id,
+                Version = BitConverter.GetBytes(1),
+                Name = "Jane Doe"
+            };
+
+            var connection = context.CreateLinqToDbConnection();
+
+            var transaction = connection.BeginTransaction();
+
+            var tempTable = connection.CreateTempTable(new List<Person> {personCopy},
                 new BulkCopyOptions {KeepIdentity = true}, "PersonUpdate");
 
             transaction.Commit();
 
             var firstPerson = tempTable.First();
 
+            tempTable.Count().Should().Be(1);
             firstPerson.Name.Should().Be(personCopy.Name);
             firstPerson.Version.Should().BeEquivalentTo(personCopy.Version, options => options.WithStrictOrdering());
         }
